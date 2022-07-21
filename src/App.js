45fe14719b835ec7d043a42efc59f11b54a60772
Ebrima Saidy <saidyebrima83@gmail.com>
2022-07-21 19:58:21 +0100

    2. Render In Table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import { Spinner } from 'react-bootstrap';
-import axios from 'axios'
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
 
 const dataUrl = "https://run.mocky.io/v3/7cb595ed-2882-4dc7-8179-d38d0b9c9d13";
 
@@ -27,11 +28,38 @@ function App() {
     {companyData === undefined ? 
     <Spinner animation='border'/>
     :
-    <ul>
+    <Table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>company</th>
+          <th>sector</th>
+          <th>stockSymbol</th>
+          <th>address</th>
+          <th>amount</th>
+          <th>currency</th>
+          <th>latitude</th>
+          <th>longitude</th>
+        </tr>
+      </thead>
+      <tbody>
+        
       {companyData.map(company => (
-        <li key={company.id}>{company.company}</li>
+        <tr>
+        <td>{company.id}</td>
+        <td>{company.company}</td>
+        <td>{company.sector}</td>
+        <td>{company.stockSymbol}</td>
+        <td>{company.address}</td>
+        <td>{company.fees.amount}</td>
+        <td>{company.fees.currency}</td>
+        <td>{company.location.latitude}</td>
+        <td>{company.location.longitude}</td>
+        </tr>
       ))}
-    </ul>
+      
+      </tbody>
+    </Table>
 }
     </>
   );
